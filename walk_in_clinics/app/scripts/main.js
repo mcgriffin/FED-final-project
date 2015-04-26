fe7f0ef916dfc3997b1cb9a44165a4eb04b48c71
Melissa Griffin <melissa.c.griffin@gmail.com>
2015-04-26 09:23:20 -0400

    tried to add maps plugin

--- walk_in_clinics/app/scripts/main.js
+++ walk_in_clinics/app/scripts/main.js
@@ -4,11 +4,16 @@
   $(document).ready(function() {
     // Add your jQuery code here
   
-    $('#float_username').floatlabel();
-    $('#float_password').floatlabel();	
+    $('.float_username').floatlabel();
+    $('.float_password').floatlabel();	
 
- 
+ 	function initialize() {
+ 		var mapOptions = {
+ 			zoom: 8,
+ 			center: new google.maps.LatLng(-34.397, 150.644)
+ 		};
 
+ 		
 
   });
 })(jQuery);
