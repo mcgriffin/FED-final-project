8172b11a29a2b2feb571a4b5ac5eea50726e0c20
Melissa Griffin <melissa.c.griffin@gmail.com>
2015-05-02 18:05:50 -0400

    removed most floatlabel references

--- walk_in_clinics/app/scripts/main.js
+++ walk_in_clinics/app/scripts/main.js
@@ -1,19 +1,24 @@
 'use strict';
 
+
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
-  
-    $('.float_username').floatlabel();
-    $('.float_password').floatlabel();	
 
- 	function initialize() {
- 		var mapOptions = {
- 			zoom: 8,
- 			center: new google.maps.LatLng(-34.397, 150.644)
- 		};
 
- 		
 
-  });
+    $(function(){
+    	$('#menu').slicknav({
+    		duration: 750,
+    		label: '',
+    	});
+    });
+    
+   
+    $('#clinician_search_field').floatlabel();
+    
+
+   
+}); 
+
 })(jQuery);
